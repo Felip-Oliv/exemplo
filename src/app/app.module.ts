ac8068269c8456890a52a50fbe6767cb9f76e777
Luiz Felipe A. Oliveira <67592458+Felip-Oliv@users.noreply.github.com>
2022-03-23 20:49:52 -0300

    Exemplo1

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AdmComponent } from './adm/adm.component';
+import { SobreComponent } from './sobre/sobre.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    AsideComponent,
+    FooterComponent,
+    HomeComponent,
+    AdmComponent,
+    SobreComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
